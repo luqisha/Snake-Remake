016070fa3f61b79c45b75a2cca0079a5f65fe0d1
Ashique <ashiqul.islam12@gmail.com>
2023-08-16 03:34:12 +0600

    Added basic score functionality

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
@@ -7,14 +9,27 @@ const platformHeight = 0.5;
 const platformWidth = 5;
 const platformDepth = 5;
 
-const snakeUnit = 0.5; // snake size big 
-const movementSpeed = 0.015;
+
+const wallThickness = 0.01; 
+const wallHeight = platformHeight + 0.1;
+const wallColor = 0x00ff00;
+const wallOpacity = 0.2;
+
+const boundaryLeft = -platformWidth / 2 - wallThickness / 2;
+const boundaryRight = platformWidth / 2 + wallThickness / 2;
+const boundaryTop = platformDepth / 2 + wallThickness / 2;
+const boundaryBottom = -platformDepth / 2 - wallThickness / 2;
+
+
+const snakeUnit = 0.1;
+var movementSpeed = 0.015;
 
 var snakeDir = 'right';
-var previousSnakeDir  = 'right';   // to store previous movement 
-var Score = 0;
-var coordinateFood=0 ;
+var score = 0;
+var teleport = false;
 let font; 
+
+const scoreObject = document.querySelector('#score');
 
 // Creating the renderer
 const renderer = new THREE.WebGLRenderer();
@@ -29,6 +44,12 @@ const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerH
 camera.position.set(0, 4, 4);
 camera.lookAt(scene.position);
 
+// Creating the control for the camera
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.dampingFactor = 0.05;
+controls.enableDamping = true;
+// controls.enableZoom = true;
+
 
 
 // Creating the platform
@@ -41,12 +62,40 @@ const platformMaterial = new THREE.MeshBasicMaterial({
 const platform = new THREE.Mesh(platformGeometry, platformMaterial);
 scene.add(platform);
 
+
+// Creating the walls around the platform
+// Left wall
+const leftWallMaterial = new THREE.MeshBasicMaterial({ color: wallColor, transparent: true, opacity: wallOpacity });
+
+const leftWallGeometry = new THREE.BoxGeometry(wallThickness, wallHeight, platformDepth);
+const leftWall = new THREE.Mesh(leftWallGeometry, leftWallMaterial);
+leftWall.position.set(-platformWidth / 2 - wallThickness / 2, 0.1, 0);
+scene.add(leftWall);
+
+// Right wall
+const rightWallMaterial = new THREE.MeshBasicMaterial({ color: wallColor, transparent: true, opacity: wallOpacity });
+const rightWallGeometry = new THREE.BoxGeometry(wallThickness, wallHeight, platformDepth);
+const rightWall = new THREE.Mesh(rightWallGeometry, rightWallMaterial);
+rightWall.position.set(platformWidth / 2 + wallThickness / 2, 0.1, 0);
+scene.add(rightWall);
+
+// Top wall
+const topWallMaterial = new THREE.MeshBasicMaterial({ color: wallColor, transparent: true, opacity: wallOpacity });
+const topWallGeometry = new THREE.BoxGeometry(platformWidth + 2 * wallThickness, wallHeight, wallThickness);
+const topWall = new THREE.Mesh(topWallGeometry, topWallMaterial);
+topWall.position.set(0, 0.1, platformDepth / 2 + wallThickness / 2);
+scene.add(topWall);
+
+// Bottom wall
+const bottomWallMaterial = new THREE.MeshBasicMaterial({ color: wallColor, transparent: true, opacity: wallOpacity });
+const bottomWallGeometry = new THREE.BoxGeometry(platformWidth + 2 * wallThickness, wallHeight, wallThickness);
+const bottomWall = new THREE.Mesh(bottomWallGeometry, bottomWallMaterial);
+bottomWall.position.set(0, 0.1, -platformDepth / 2 - wallThickness / 2);
+scene.add(bottomWall);
+
+
+
 // Creating the snake object
-
-
-
-
-
 const snakeGeometry = new THREE.BoxGeometry(snakeUnit, snakeUnit, snakeUnit);
 const snakeMaterial = new THREE.MeshBasicMaterial({ color: 0x567AFF });
 const snake = new THREE.Mesh(snakeGeometry, snakeMaterial);
@@ -54,7 +103,7 @@ snake.position.set(0, platformHeight/2, 0);
 scene.add(snake);
 
 // Creating the food object
-const foodGeometry = new THREE.CircleGeometry(snakeUnit/1.5, 200);
+const foodGeometry = new THREE.SphereGeometry(snakeUnit/1.5, 200, 200);
 const foodMaterial = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
 const food = new THREE.Mesh(foodGeometry, foodMaterial);
 
@@ -64,11 +113,18 @@ var randomCoord = getRandomCoord();
 console.log(randomCoord);
 var foodX = randomCoord.x;
 var foodZ = randomCoord.z;
-food.position.set(foodX, snake.position.y, foodZ);
+food.position.set(foodX, snake.position.y + snakeUnit/1.5/2, foodZ);
 scene.add(food);
 
 
 // Creating text object for showing score
+// const fontLoader = new FontLoader();
+// const fontPath = './fonts/Nasalization Rg_Regular.json';
+
+// fontLoader.load(fontPath, (loadedFont) => {
+//   font = loadedFont;
+// });
+
 // var textString = '0';
 // var textGeometry = new TextGeometry(textString, {
 //   font: font,
@@ -89,149 +145,181 @@ renderer.render(scene, camera);
 
 // Defining key press events
 function onKeyDown(event) {
-    switch (event.keyCode) {
-      case 65: // A Key
-        snakeDir = 'left';
-        console.log('left');
-        break;
-      case 68: // D Key
-        snakeDir = 'right';
-        console.log('right');
-        break;
-      case 87: // W Key
-        snakeDir = 'up';
-        console.log('up');
-        break;
-      case 83: // S Key
-        snakeDir = 'down';
-        console.log('down');
-        break;
-    }
+  switch (event.keyCode) {
+    case 65: // A Key
+      snakeDir = 'left';
+      break;
+    case 68: // D Key
+      snakeDir = 'right';
+      break;
+    case 87: // W Key
+      snakeDir = 'up';
+      break;
+    case 83: // S Key
+      snakeDir = 'down';
+      break;
+  }
 };
 
 document.addEventListener('keydown', onKeyDown, false);
 
-var coordinateFood=4 ;
+
+function updateScore(){
+  if(score < 0){
+    scoreObject.style.display = 'none';
+    GameOver(score);
+  }
+  else
+  {
+    scoreObject.innerHTML = `
+    points: ${score}`
+
+    if(score > 10 && score < 20){
+      movementSpeed = movementSpeed*1.5;
+    }
+    else if(score > 20 && score < 30){
+      movementSpeed = movementSpeed*2;
+    }
+    else if(score > 30 ){
+      movementSpeed = movementSpeed*2.5;
+    }
+
+  }
+}
+
 // Function to get random coordinates from the platform
 function getRandomCoord() {
-  var value = (Math.random()*10);
-  var coordinateFood=Math.ceil(value) ;                               // random value generate for different cordinates 
-  if (coordinateFood % 4 == 0){
-    var X = (Math.random() * (platformWidth/2)) - (platformWidth/2);
-    var Y = (Math.random() * (platformHeight/2)) - (platformHeight/2);
-    var Z = (Math.random() * (platformDepth/2)) - (platformDepth/2);
-  }
-  else if (coordinateFood % 4 == 1){
-    var X = -((Math.random() * (platformWidth/2)) - (platformWidth/2));
-    var Y = (Math.random() * (platformHeight/2)) - (platformHeight/2);
-    var Z = (Math.random() * (platformDepth/2)) - (platformDepth/2);
-  }
-  else if (coordinateFood % 4 == 2){
-    var X = (-Math.random() * (platformWidth/2)) + (platformWidth/2);
-    var Y = (-Math.random() * (platformHeight/2)) + (platformHeight/2);
-    var Z = (-Math.random() * (platformDepth/2)) + (platformDepth/2);
-  }
-  else if (coordinateFood % 4 == 3){
-    var X = (Math.random() * (platformWidth/2)) - (platformWidth/2);
-    var Y = -(Math.random() * (platformHeight/2)) + (platformHeight/2);
-    var Z = -(Math.random() * (platformDepth/2)) + (platformDepth/2);
-  }
+  var X = (Math.random() * platformWidth) - platformWidth/2;
+  var Y = (Math.random() * platformHeight/2) + platformHeight/4;
+  var Z = (Math.random() * platformDepth) - platformDepth/2;
+
   return { x: X, y: Y, z: Z };
 };
 
 
 // Function to check collision
 function checkCollision(object1, object2) {
-    const box1 = new THREE.Box3().setFromObject(object1);
-    const box2 = new THREE.Box3().setFromObject(object2);
-  
-    return box1.intersectsBox(box2);
+  const box1 = new THREE.Box3().setFromObject(object1);
+  const box2 = new THREE.Box3().setFromObject(object2);
+
+  return box1.intersectsBox(box2);
 };
   
 
 // Function to destroy objects
 function destroyObject(object) {
-    if (object.parent) {
-      object.parent.remove(object);
-    }
-  
-    if (object.material) {
-      object.material.dispose();
-  
-      if (object.material.map) {
-        object.material.map.dispose();
-      }
-    }
-  
-    if (object.geometry) {
-      object.geometry.dispose();
-    }
+  if (object.parent) {
+    object.parent.remove(object);
+  }
 
-    object = null;
+  if (object.material) {
+    object.material.dispose();
+
+    if (object.material.map) {
+      object.material.map.dispose();
+    }
+  }
+
+  if (object.geometry) {
+    object.geometry.dispose();
+  }
+
+  object = null;
 };
 
 
-// const fontLoader = new THREE.FontLoader();
-// const fontUrl = './fonts/Nasalization Rg_Regular.json';
 
-// fontLoader.load(fontUrl, (loadedFont) => {
-//   font = loadedFont;
-// });
+function resetWallColor(){
+  score -= 1;
+  setTimeout(() => {
+    leftWall.material.color.set(0x00ff00);
+    rightWall.material.color.set(0x00ff00);
+    topWall.material.color.set(0x00ff00);
+    bottomWall.material.color.set(0x00ff00);
+  }, 1000);
+  
+}
 
+function GameOver(score) {
+  cancelAnimationFrame(animate);
+  scene.visible = false;
+
+  const gameOverScreen = document.getElementById('game-over-screen');
+  const gameOverScore = document.getElementById('game-over-score');
+
+  if(score < 0){
+    gameOverScore.textContent = `Score: 😭`;
+  }
+  else
+  {
+    gameOverScore.textContent = `Score: ${score}`;
+  }
+  gameOverScreen.style.display = 'block';
+}
   
 
 // Rendering the scene continuously
 function animate() {
-    requestAnimationFrame(animate);
- 
-    if(snakeDir == 'right' && previousSnakeDir != 'left'){
-        snake.position.x += movementSpeed;
-        previousSnakeDir = 'right';
-    }
-    else if(previousSnakeDir == 'right' && snakeDir == 'left'){
-        snake.position.x += movementSpeed;
-        previousSnakeDir = 'right';
-    }
-    else if(snakeDir == 'left' && previousSnakeDir != 'right'){
-        snake.position.x -= movementSpeed;
-        previousSnakeDir = 'left';
-    }
-    else if(previousSnakeDir == 'left' && snakeDir == 'right'){
-      snake.position.x -= movementSpeed;
-      previousSnakeDir = 'left';
-    }
-    else if(snakeDir == 'up' && previousSnakeDir != 'down' ){
-        snake.position.z -= movementSpeed;
-        previousSnakeDir = 'up';
-    }
-    else if(previousSnakeDir == 'up' && snakeDir == 'down'){
-        snake.position.z -= movementSpeed;
-        previousSnakeDir = 'up';
-    }
-    else if(snakeDir == 'down' && previousSnakeDir != 'up' ){
-        snake.position.z += movementSpeed;
-        previousSnakeDir = 'down';
-    }
-    else if(previousSnakeDir == 'down' && snakeDir == 'up'){
-        snake.position.z += movementSpeed;
-        previousSnakeDir = 'down';
-    }
+  requestAnimationFrame(animate);
+  controls.update();
+  updateScore();
+
+  if(teleport == true){
+    resetWallColor();
+  }
+
+  if(snakeDir == 'right'){
+    snake.position.x += movementSpeed;
+  }
+  else if(snakeDir == 'left'){
+    snake.position.x -= movementSpeed;
+  }
+  else if(snakeDir == 'up'){
+    snake.position.z -= movementSpeed;
+  }
+  else if(snakeDir == 'down'){
+    snake.position.z += movementSpeed;
+  }
 
 
-    if(checkCollision(snake, food)){
+  if (checkCollision(snake, leftWall)) {
+    teleport = true;
+    leftWall.material.color.set(0xff0000);  
+    snake.position.x = boundaryRight - snakeUnit / 2;
+  } 
+  else if (checkCollision(snake, rightWall)) {
+    teleport = true;
+    rightWall.material.color.set(0xff0000);
+    snake.position.x = boundaryLeft + snakeUnit / 2;
+  } 
+  else if (checkCollision(snake, topWall)) {
+    teleport = true;
+    topWall.material.color.set(0xff0000);
+    snake.position.z = boundaryBottom + snakeUnit / 2;
+  } 
+  else if (checkCollision(snake, bottomWall)) {
+    teleport = true;
+    topWall.material.color.set(0xff0000);
+    snake.position.z = boundaryTop - snakeUnit / 2;
+  }
+  else
+  {
+    teleport = false;
+  }
+    
 
-        
-        randomCoord = getRandomCoord(); 
-        
-        console.log(randomCoord);
-        foodX = randomCoord.x;
-        foodZ = randomCoord.z;
-        food.position.set(foodX, snake.position.y, foodZ);
-        Score += 1;
-        console.log(Score);
-    }
-
-    renderer.render(scene, camera);
+  if(checkCollision(snake, food)){
+      randomCoord = getRandomCoord(); 
+      
+      console.log(randomCoord);
+      foodX = randomCoord.x;
+      foodZ = randomCoord.z;
+      food.position.set(foodX, snake.position.y, foodZ);
+      score += 1;    
+  }
+  
+  
+  renderer.render(scene, camera);
 };
 
 animate();
